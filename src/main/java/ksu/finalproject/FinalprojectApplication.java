709ccef193d78bc4f3cedf1c5be3a33ee493d7b0
feralshining <racc00ns@naver.com>
2026-04-01 13:59:00 +0900

    chore: 프로젝트 초기 설정 - .gitignore 설정 - build.gradle 의존성 추가 (Spring Security, JPA, H2, JWT, Validation, Lombok) - application.properties H2 인메모리 DB, JPA, JWT 설정 - @EnableJpaAuditing 추가

--- src/main/java/ksu/finalproject/FinalprojectApplication.java
+++ src/main/java/ksu/finalproject/FinalprojectApplication.java
@@ -2,8 +2,10 @@ package ksu.finalproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // annotation 자동 감시
 public class FinalprojectApplication {
 
     public static void main(String[] args) {

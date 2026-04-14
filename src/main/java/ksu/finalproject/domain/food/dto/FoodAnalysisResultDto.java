19e0c884b9be6667dc297e90bef45b07ff8f80e3
feralshining <racc00ns@naver.com>
2026-04-14 10:32:14 +0900

    refactor: analysisStatus의 타입을 enum으로 변경

--- src/main/java/ksu/finalproject/domain/food/dto/FoodAnalysisResultDto.java
+++ src/main/java/ksu/finalproject/domain/food/dto/FoodAnalysisResultDto.java
@@ -1,6 +1,7 @@
 package ksu.finalproject.domain.food.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import ksu.finalproject.domain.food.entity.enums.AnalysisStatus;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,7 +16,7 @@ import java.util.List;
 @AllArgsConstructor
 public class FoodAnalysisResultDto {
     @JsonProperty("analysis_status")
-    private String analysisStatus;
+    private AnalysisStatus analysisStatus;
 
     @JsonProperty("model_version")
     private String modelVersion;
